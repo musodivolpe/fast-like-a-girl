a6f8b6ab17a28bf727d61ba77655d34f5d00491b
musodivolpe <85736463+musodivolpe@users.noreply.github.com>
2025-09-05 20:49:27 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'ciclo-digiuno-v1';
+const ASSETS = [
+  './',
+  './index.html',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k!==CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(resp => resp).catch(() => cached))
+  );
+});
